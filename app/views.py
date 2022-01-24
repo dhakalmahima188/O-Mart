ec600de7a1d49c098f89ffc325ac6c64b56bc365
mahima <076bct033.mahima@pcampus.edu.np>
2022-01-24 16:38:35 +0545

    get request worked

--- app/views.py
+++ app/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
     return render(request,'home.html',{'title':'Home'})
-
+    
+def add(request):
+    val1=int(request.GET['a'])
+    val2=int(request.GET['b'])
+    res=val1+val2
+    print(res)
+    return render(request,'add.html',{'result':res})
