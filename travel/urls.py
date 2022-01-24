7479c7e27fb11e6b7dab9b31ccb2c16ba4d1a9b4
mahima <076bct033.mahima@pcampus.edu.np>
2022-01-24 23:08:48 +0545

    loaded template of travello

--- travel/urls.py
+++ travel/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('',include('app.urls')),
+    path('',include('tourism.urls')),
     path('admin/', admin.site.urls),
 ]
