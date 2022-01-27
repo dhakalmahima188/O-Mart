243f65ec241f8352d43be9f324c64ecbc5757429
mahima <076bct033.mahima@pcampus.edu.np>
2022-01-27 22:23:36 +0545

    assests loaded

--- app/views.py
+++ app/views.py
@@ -4,8 +4,8 @@ def home(request):
     return render(request,'home.html',{'title':'Home'})
     
 def add(request):
-    val1=int(request.POST['a'])
-    val2=int(request.POST['b'])
+    val1=int(request.GET['a'])
+    val2=int(request.GET['b'])
     res=val1+val2
     print(res)
     return render(request,'add.html',{'result':res})
--- tourism/views.py
+++ tourism/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'price':'800','name':'Lumbini','desc':'Lumbini is a holy place.'})
