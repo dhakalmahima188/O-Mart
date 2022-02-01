3d5713a92221077157df2073ab515fe4457c561d
mahima <076bct033.mahima@pcampus.edu.np>
2022-02-01 17:42:21 +0545

    messAGES DISPLAYED

--- account/views.py
+++ account/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import redirect, render,HttpResponse,redirect
 from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
     # return HttpResponse("hello world")
 
-    if request.method == 'POST':
+    if (request.method == 'POST'):
         print("Post wala")
         first_name = request.POST['first_name']  
         username = request.POST['username']
         password = request.POST['password']
         email = request.POST['email']
         if User.objects.filter(username=username).exists():
+            messages.info(request,"Username Taken")
             print("Username exists")
-            return redirect('/account/register')
+            return redirect('register')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,"Email Taken")
+            print("Username exists")
+            return redirect('register')
         else:
             user=User.objects.create_user(username=username,password=password,first_name=first_name,email=email)
-            user.save();
+            user.save()
             print("USer created")
             return redirect('/')
     else: 
