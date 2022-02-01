d83e5ffcf31b2dbbf0cd2fdbef5528e933889f25
mahima <076bct033.mahima@pcampus.edu.np>
2022-02-01 16:55:22 +0545

    unique username

--- account/views.py
+++ account/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import redirect, render,HttpResponse
+from django.shortcuts import redirect, render,HttpResponse,redirect
 from django.contrib.auth.models import User,auth
+
 # Create your views here.
 def register(request):
     # return HttpResponse("hello world")
 
     if request.method == 'POST':
-        first_name = request.POST['fname']
-        last_name = request.POST['lname']
-        username = request.POST['uname']
+        print("Post wala")
+        first_name = request.POST['first_name']  
+        username = request.POST['username']
         password = request.POST['password']
         email = request.POST['email']
-
-        user=User.objects.create(username=username,password=password,first_name=first_name,last_name=last_name,email=email)
-        user.save()
-        print("USer created")
-        return redirect('/')
-    else:        
+        if User.objects.filter(username=username).exists():
+            print("Username exists")
+            return redirect('/account/register')
+        else:
+            user=User.objects.create_user(username=username,password=password,first_name=first_name,email=email)
+            user.save();
+            print("USer created")
+            return redirect('/')
+    else: 
+        print("gayena hai")       
         return render(request, 'register.html')
